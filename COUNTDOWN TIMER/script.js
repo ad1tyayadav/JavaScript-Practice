6e6b98d145c78a2ad5c5ec370865c267683c7334
Adii Code <adityaproworks@gmail.com>
2024-05-23 02:59:14 +0530

    Added initial project files for Countdown Timer

---
+++ COUNTDOWN TIMER/script.js
@@ -0,0 +1,25 @@
+const endDate = "27 January 2024 10:00 PM"
+
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.getElementsByTagName("input");
+
+
+function clock() {
+    let end = new Date(endDate);
+    let now = new Date();
+
+    let diff = Math.floor((end - now) / 1000);
+
+    if (diff < 0) return;
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+    inputs[1].value = Math.floor(diff / 3600) % 24;
+    inputs[2].value = Math.floor(diff / 60) % 60;
+    inputs[3].value = Math.floor(diff) % 60;
+}
+
+setInterval(
+    () => {
+        clock();
+    },
+    1000
+);
